5e691b226c0e68c9187eb3752a2feace04205708
luxueyan <xueyan.lu@bitmain.com>
2019-07-18 17:27:32 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+(function(win, doc) {
+	const $ = doc.querySelector.bind(doc);
+	win.onload = function() {
+		doc.onkeyup = function(e) {
+			const iframe = $("#mce_0_ifr");
+			if (!iframe) return;
+			if (e.altKey && e.keyCode === 68 /*c*/) {
+				const iframeDoc = iframe.contentDocument;
+				iframeDoc.body.innerHTML = `
+					<p>
+						说明：
+					</p>
+					<p>
+						时间：${new Date().toLocaleString()}
+					</p>
+					<p>
+						版本：test分支
+					</p>
+				`;
+			}
+		};
+	};
+	win.onbeforeunload = function() {
+		doc.onkeyup = null;
+	};
+})(window, document);
