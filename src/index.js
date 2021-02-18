3e02fdb93893c2c6b7abcd9e84a9cc070a22ec2b
luxueyan <luxueyan200888@gmail.com>
2021-02-18 16:36:56 +0800

    feat: 添加模拟回车功能，避免不填写按钮是灰色的问题

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
-(function(win, doc) {
-	const $ = doc.querySelector.bind(doc);
+;(function(win, doc) {
+	function fireKeyEvent(el, evtType, keyCode) {
+		let evtObj
+		if (document.createEvent) {
+			if (window.KeyEvent) {
+				//firefox 浏览器下模拟事件
+				evtObj = document.createEvent('KeyEvents')
+				evtObj.initKeyEvent(evtType, true, true, window, true, false, false, false, keyCode, 0)
+			} else {
+				//chrome 浏览器下模拟事件
+				evtObj = document.createEvent('UIEvents')
+				evtObj.initUIEvent(evtType, true, true, window, 1)
+
+				delete evtObj.keyCode
+				if (typeof evtObj.keyCode === 'undefined') {
+					//为了模拟keycode
+					Object.defineProperty(evtObj, 'keyCode', { value: keyCode })
+				} else {
+					evtObj.key = String.fromCharCode(keyCode)
+				}
+
+				if (typeof evtObj.ctrlKey === 'undefined') {
+					//为了模拟ctrl键
+					Object.defineProperty(evtObj, 'ctrlKey', { value: true })
+				} else {
+					evtObj.ctrlKey = true
+				}
+			}
+			el.dispatchEvent(evtObj)
+		} else if (document.createEventObject) {
+			//IE 浏览器下模拟事件
+			evtObj = document.createEventObject()
+			evtObj.keyCode = keyCode
+			el.fireEvent('on' + evtType, evtObj)
+		}
+	}
+
+	const $ = doc.querySelector.bind(doc)
 	win.onload = function() {
 		doc.onkeyup = function(e) {
-			const iframe = $(".mce-edit-area iframe");
-			if (!iframe) return;
-			const iframeDoc = iframe.contentDocument;
+			const iframe = $('.mce-edit-area iframe')
+			if (!iframe) return
+			const iframeDoc = iframe.contentDocument
 			if (e.altKey && e.keyCode === 68 /*D*/) {
 				iframeDoc.body.innerHTML = `
 					<p>
@@ -16,7 +52,7 @@
 					<p>
 						版本：test-2.0分支
 					</p>
-				`;
+				`
 			} else if (e.altKey && e.keyCode === 65 /*A*/) {
 				iframeDoc.body.innerHTML = `
 					<p>
@@ -28,7 +64,7 @@
 					<p>
 						版本：test-2.0 & test-2.5分支
 					</p>
-				`;
+				`
 			} else if (e.altKey && e.keyCode === 66 /*B*/) {
 				iframeDoc.body.innerHTML = `
 					<p>
@@ -40,11 +76,12 @@
 					<p>
 						版本：test-2.5分支
 					</p>
-				`;
+				`
 			}
-		};
-	};
+			fireKeyEvent(iframeDoc.body, 'keyup', 13)
+		}
+	}
 	win.onbeforeunload = function() {
-		doc.onkeyup = null;
-	};
-})(window, document);
+		doc.onkeyup = null
+	}
+})(window, document)
